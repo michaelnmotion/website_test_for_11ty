c64ab7e7c42411066c25419493408b4ed850a374
Michael Vincent <mwjvincent@gmail.com>
2025-12-31 23:03:37 +1100

    udpate

--- .eleventy.js
+++ .eleventy.js
@@ -1,15 +1,16 @@
 module.exports = function (eleventyConfig) {
-  // Copy assets straight to output root
-  eleventyConfig.addPassthroughCopy({ "src/assets": "/" });
-  eleventyConfig.addPassthroughCopy({ "src/static": "/" });
+  // Copy assets into the output
+  eleventyConfig.addPassthroughCopy({ "src/assets": "assets" });
+  eleventyConfig.addPassthroughCopy({ "src/static": "static" });
 
   return {
+    pathPrefix: "/mvpt_site/", // <-- change to your repo name
     dir: {
       input: "src",
       includes: "_includes",
       data: "_data",
-      output: "dist"
+      output: "dist",
     },
-    templateFormats: ["njk"]
+    templateFormats: ["njk"],
   };
 };
